bd3eee8f3fc3a31fbe24978c1e89f43bab5553d7
vishnuarjun512 <vishnuarjun512@gmail.com>
2024-01-19 07:41:46 +0530

    fix:OrderItem Schema Registration and Retrieval

--- actions/giveOrderDetails.ts
+++ actions/giveOrderDetails.ts
@@ -1,14 +1,16 @@
 import Order from "@/models/order.model";
+import OrderItem from "@/models/orderItem.model";
+import Product from "@/models/product.model";
 
 const giveOrderDetails = async (storeId:string) => {
     const orders = await Order.find({
         storeId
       }).populate({
         path: "orderItems",
-        model: "OrderItem",
+        model: OrderItem,
         populate: {
           path: "productId",
-          model: "Product",
+          model: Product,
         },
       });
       return orders
